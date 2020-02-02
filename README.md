f6f4a2f7551848cae77e3a170ac3d3efce205a58
winter story <zhangzq@pkusoft.net>
2020-02-02 15:04:52 +0800

    -
    -

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spring-boot-websocket
  spring-boot-websocket
+
+spring boot 整合 websocket 实现在线聊天
---
+++ src/main/java/com/zzq/websocket/WebSocketController.java
@@ -0,0 +1,27 @@
+package com.zzq.websocket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebSocketController {
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("/websocketredi/{name}")
+    public String webSocket(@PathVariable String name, Model model) {
+        try {
+            log.info("跳转到websocket的页面上");
+            model.addAttribute("username", name);
+            return "websocket";
+        } catch (Exception e) {
+            log.info("跳转到websocket的页面上发生异常，异常信息是：" + e.getMessage());
+            return "error";
+        }
+    }
+
+}
